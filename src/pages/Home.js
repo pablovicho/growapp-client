609fc6d715203a71e08eba6f33efef86bef32fcd
pablo valdés <pablovvb@gmail.com>
2022-01-02 20:18:04 -0600

    tried navigate instead of link at home. I'll see if it works on deploy

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import React, {useContext} from 'react'
 import UserContext from '../context/User/UserContext'
 import logo3 from "../images/logo3.png"
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 
 
@@ -9,6 +9,7 @@ export default function Home() {
   const ctxUser = useContext(UserContext);
   const { singleUser, authStatus } = ctxUser;
   const { nombre, _id } = singleUser;
+  const navigate = useNavigate()
 
 
 	return (
@@ -36,16 +37,14 @@ export default function Home() {
             </div>
           </div>
 
-      <Link to = {`../profile/${_id}`}>
           <div className="px-6 py-0">
             <div className="font-bold text-3xl mb-2 text-yellow-900 justify-center mt-8">
-            <button type="submit"
+            <button type="submit" onClick={() => {navigate(`../profile/${_id}`)}}
                   className="my-5 bg-lime-600 border w-40 border-transparent rounded-md shadow-sm py-2 px-4 inline-flex justify-center text-sm font-medium text-white hover:bg-lime-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
                   VER TU PERFIL
           </button>
             </div>
           </div>
-      </Link>
           </>
 :
           <div className="px-6 py-4">
